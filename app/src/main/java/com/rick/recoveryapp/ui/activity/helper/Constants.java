149a9834790b18113547003342d8d04839802cb0
user <942772119@qq.com>
2023-11-10 09:39:59 +0800

    优化

--- app/src/main/java/com/rick/recoveryapp/ui/activity/helper/Constants.java
+++ app/src/main/java/com/rick/recoveryapp/ui/activity/helper/Constants.java
@@ -2,14 +2,11 @@ package com.rick.recoveryapp.ui.activity.helper;
 
 public interface Constants {
     String BT_CONNECTED = "BT_CONNECTED";//连接状态
-
-    String BLUETOOTH_DISCONNECTS = "BLUETOOTH_DISCONNECTS";//蓝牙断开
     String BT_PROTOCOL = "BT_PROTOCOL";
     String BT_ECG = "BT_ECG";
     String BT_RECONNECTED = "BT_RECONNECTED";//重新连接
     String active = "主动模式";
     String passive = "被动模式";
     String intelligence = "智能模式";
-    String Mac_Bt_Update = "Mac_Bt_Update";//蓝牙连接
 
 }
